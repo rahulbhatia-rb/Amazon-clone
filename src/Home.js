8259728a84a96d0608eabc57da2a153fb462a8ca
Rahul H Bhatia <rahul_harikishan@srmuniv.edu.in>
2020-09-27 15:28:08 +0530

    Flex-Columnnar

--- src/Home.js
+++ src/Home.js
@@ -9,7 +9,7 @@ function Home() {
             </div>
             <div className="home_row">
                 <Product/>
-                {/* <Product /> */}
+                <Product />
             </div>
             <div className="home_row">
                 {/* <Product />
